f9c0b182452706bb28cc687f9053a96541333431
Dhruv <62509177+darkn3to@users.noreply.github.com>
2025-04-20 13:53:21 +0530

    tested

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char **argv) {
     ext2_filsys fs;
-    const char *device_partition = "/dev/sda9";
+    const char *device_partition = "/dev/sda10";
     errcode_t errcode = ext2fs_open(device_partition, 0, 0, 0, unix_io_manager, &fs);
 
     if (errcode) {
